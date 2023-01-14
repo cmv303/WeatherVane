1ec4fbdd55cd17c3b41bf3fc60c70da3cbd2b65f
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-01-14 16:29:32 -0500

    included a lot of basic and boilerplate information for html, css, and js files

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+//*api key
+// 1dbbe04e7eaff33cabdcdc99e3464ff7
+
+//TODO functions
+// function searchCity() {
+
+// }
+
+
+//TODO api calls
+//*5 day forecast
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+//*current forecast
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+
+//TODO local storage
+//search bar
+//previously searched
