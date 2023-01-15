9668d47fd4a4e0d36d707df4e511b71421755379
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-01-15 16:05:32 -0500

    cleaned up js, still cannot get fetchto work, but at least got jquery to load properly

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,15 @@
-//*api key
+//api key
 // 1dbbe04e7eaff33cabdcdc99e3464ff7
 
 // variables for form inputs
 const form = document.getElementById("weather-form");
 const input = document.getElementById("city-input");
 const searchHistory = document.getElementById("search-history");
+const searchButton = document.getElementById("search-btn");
 
 // Search for City
 $(document).ready(function () {
+  console.log("ready!")
   function searchCity(currentCity) {
     $("#search-btn").on("click", function () {
       //fetches api for current weather
@@ -25,7 +27,7 @@ $(document).ready(function () {
 
       //fetches api for 5-day forecast
       fetch(
-        `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=1dbbe04e7eaff33cabdcdc99e3464ff7`
+        `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=1dbbe04e7eaff33cabdcdc99e3464ff7`
       )
         .then(function (response) {
           return response.json();
@@ -38,44 +40,6 @@ $(document).ready(function () {
 });
 
 // TO CONTINUE
-
-//     // AND that city is added to the search history
-//   const citySearchedHistory = document.createElement("a");
-//   citySearchedHistory.href = "#";
-//   citySearchedHistory.innerText = currentCity;
-//   citySearchedHistory.addEventListener("click", (e) => {
-//     e.preventDefault();
-
-//   });
-//   searchHistory.appendChild(citySearchedHistory);
-// });
-
-// // WHEN I view current weather conditions for that city
-// // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-// function displayCurrentWeather(data) {
-//     const cityName = document.getElementById("city-name");
-//     cityName.innerText = data.name;
-//     const date = document.getElementById("date");
-//     date.innerText = new Date().toLocaleDateString();
-//     const icon = document.getElementById("icon");
-//     icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-//     const temperature = document.getElementById("temperature");
-//     temperature.innerText = data.main.temp;
-//     const humidity = document.getElementById("humidity");
-//     humidity.innerText = data.main.humidity;
-//     const windSpeed = document.getElementById("wind-speed");
-//     windSpeed.innerText = data.wind.speed;
-
-// // WHEN I view future weather conditions for that city
-//   // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-//   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=1dbbe04e7eaff33cabdcdc99e3464ff7`)
-//   .then(response => response.json())
-//   .then(forecastData => {
-//     const forecast = forecastData.list;
-//     for (let i = 0; i < forecast.length; i++) {
-//       const forecastDay = forecast[i];
-//       const forecastContainer = document.getElementById(`forecast-${i}`);
-//     }}
 
 //TODO functions
 // function searchCity() {
