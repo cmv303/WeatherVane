906c4362e362af5d0b9b0b18b727d026cd2c0720
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-01-28 14:56:38 -0500

    working on fetch to work

--- Assets/script.js
+++ Assets/script.js
@@ -6,57 +6,13 @@ let searchInput = document.querySelector("#searchBar")
 let currentContainer = document.querySelector(".current-weather");
 let forecastContainer = document.querySelector(".five-day-forecast");
 let searchHistoryContainer = document.querySelector(".history");
-dayjs.extend(window.dayjs_plugin_utc)
-dayjs.extend(window.dayjs_plugin_timezone)
 
-//function to display searchHistoryList
-function renderSearchHistory() {
-  searchHistoryContainer.innerHTML = "";
-  for (let i = searchHistory.length - 1; i >=0; i--){
-    let btn = document.createElement("button")
-    btn.setAttribute("type", "button")
-    btn.setAttribute("class", "history-btn")
-    btn.setAttribute("data-search", searchHistory[i])
-    btn.textContent = searchHistory[i]
-    searchHistoryContainer.append(btn)
-  }
-}
-
-//function to update history in Local Storage and update displayed history
-function addToHistory(search) {
-  if (searchHistory.indexOf(search) !== -1) {
-    return
-  } searchHistory.push(search);
-  localStorage.setItem("search-history", JSON.stringify(searchHistory))
-  renderSearchHistory();
-}
-//function to get search history from Local Storage
-function getHistory() {
-  let storageHistory = localStorage.getItem("search-history")
-  if (storageHistory) {
-    searchHistory = JSON.parse(storageHistory)
-  }
-  renderSearchHistory();
-}
-//function to display currentWeatherData fetched from api
-function renderCurrentWeather(city,weather) {
-  //variable for date
-  //variable for all data points (temp, humidity, icon, etc)
-  //create html elements in JS that build the structure of how i want that to look
-  //set their attributes
-  //set their textcontent --this references variables for all data points
-}
-
-//function to display fiveDayForecast
-//follow above process but doing it for 5 cards instead of one let temp1, let temp2 .... let wind1, let wind2....
-
-//function that runs the two previous functions
-function renderItems(city,data) {
-  renderCurrentWeather(city, data.list[0], data.city.timezone)
-  fiveDayForecast(data.list);
-}
-
-//function that calls api and gives us all the data we need for the currentWeather and Forecast
+//gets document ready
+  $(document).ready(function () {
+    $("#search-btn").on("click", function () {
+//event listener for search button
+      console.log("Search button clicked!")
+//fetch api info for current weather (and Forecast??)
 function fetchCurrentWeather(location) {
   let {lat} = location
   let {lon} = location
@@ -74,6 +30,115 @@ function fetchCurrentWeather(location) {
       console.error("Error fetching weather data: " + error);
     });
 }
+//display (render) current info for current weather
+function displayCurrentWeather(city,fetchweather) {
+  const currentDate = new(date);
+  //variable for all data points (temp, humidity, icon, etc)
+  let currentDataPoints;
+  //create html elements in JS that build the structure of how i want that to look
+  //set their attributes
+  //set their textcontent --this references variables for all data points
+}
+//be able to clear search
+
+//set displayed (rendered) info into local history
+function addSearchToHistory(search) {
+  if (searchHistory.indexOf(search) !== -1) {
+    return
+  } searchHistory.push(search);
+  localStorage.setItem("search-history", JSON.stringify(searchHistory))
+  renderSearchHistory();
+}
+//fetch api info for 5day forecast
+//event listener for 5day search button
+
+//display 5day forecast
+function displayFiveDayForecast() {
+  forecastContainer.innerHTML = "";
+  for (let i = forecast.length - 1; i >=0; i--){
+    let btn = document.createElement("button")
+    btn.setAttribute("type", "button")
+    btn.setAttribute("class", "history-btn")
+    btn.setAttribute("data-search", forecast[i])
+    btn.textContent = searchHistory[i]
+    forecastContainer.append(btn)
+  }
+}
+
+//connect displayed 5day to addSearchToHistory to set locally
+
+//get searchHistory from local storage
+function getSearchHistory() {
+  let storageHistory = localStorage.getItem("search-history")
+  if (storageHistory) {
+    searchHistory = JSON.parse(storageHistory)
+  }
+  renderSearchHistory();
+}
+
+//display searchHistoryList
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = "";
+  for (let i = searchHistory.length - 1; i >=0; i--){
+    let btn = document.createElement("button")
+    btn.setAttribute("type", "button")
+    btn.setAttribute("class", "history-btn")
+    btn.setAttribute("data-search", searchHistory[i])
+    btn.textContent = searchHistory[i]
+    searchHistoryContainer.append(btn)
+  }
+}
+
+  })});
+
+
+
+
+
+
+
+
+
+
+
+
+//follow above process but doing it for 5 cards instead of one let temp1, let temp2 .... let wind1, let wind2....
+function renderFiveDayForecast() {
+let temp1 = "";
+let temp2 = "";
+let temp3 = "";
+let temp4 = "";
+let temp5 = "";
+
+let wind1 = "";
+let wind2 = "";
+let wind3 = "";
+let wind4 = "";
+let wind5 = "";
+
+let humidity1 = "";
+let humidity2 = "";
+let humidity3 = "";
+let humidity4 = "";
+let humidity5 = "";
+
+let icon1 = "";
+let icon2 = "";
+let icon3 = "";
+let icon4 = "";
+let icon5 = "";
+
+let forecastDataPoints;
+
+}
+
+//function that runs the two previous functions
+function renderItems(city,data) {
+  renderCurrentWeather(city, data.list[0], data.city.timezone)
+  renderFiveDayForecast(data.list);
+}
+
+
 
 //function that calls the api and gives us the coordinants
 function fetchCoords(search) {
@@ -102,32 +167,29 @@ function fetchCoords(search) {
 
 
 
-// $(document).ready(function () {
-  $("#search-btn").on("click", function () {
-    console.log("Search button clicked!");
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        fetchCurrentWeather(position.coords);
-      }, function(error) {
-        console.log("Geolocation failed: " + error.message);
-      });
-    } else {
-      console.log("Geolocation is not supported by your browser.");
-    }
-    });
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(function (position) {
+  //       fetchCurrentWeather(position.coords);
+  //     }, function(error) {
+  //       console.log("Geolocation failed: " + error.message);
+  //     });
+  //   } else {
+  //     console.log("Geolocation is not supported by your browser.");
+  //   }
+  // //   
 
     
-    function displayCurrentWeather(data) {
-      const city = document.getElementById("city");
-      city.innerHTML = data.name; //!only shows using location in console
-      const climateDescription = document.getElementById("climateDescription"); //!Not showing up
-      climateDescription.innerHTML - data.weather[0].main;
-      const tempValue = document.getElementById("tempValue");
-      tempValue.innerHTML = Math.round(data.main.feels_like - 273.15) + " °C"
-      // const humidity = document.getElementById("humidity");
-      // humidity.innerHTML = data.humidity
-      // const windSpeed = document.getElementById("windSpeed");
-      // windSpeed.innerHTML = 
-    }
-  });
+  //   function displayCurrentWeather(data) {
+  //     const city = document.getElementById("city");
+  //     city.innerHTML = data.name; //!only shows using location in console
+  //     const climateDescription = document.getElementById("climateDescription"); //!Not showing up
+  //     climateDescription.innerHTML - data.weather[0].main;
+  //     const tempValue = document.getElementById("tempValue");
+  //     tempValue.innerHTML = Math.round(data.main.feels_like - 273.15) + " °C"
+  //     // const humidity = document.getElementById("humidity");
+  //     // humidity.innerHTML = data.humidity
+  //     // const windSpeed = document.getElementById("windSpeed");
+  //     // windSpeed.innerHTML = 
+  //   }
+  // });
 
