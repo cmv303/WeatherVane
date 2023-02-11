eb3d0c531f1f1b3bae03483e27b1af9b0fb782fd
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-02-11 13:57:29 -0500

    refactored js and worked on css styling

--- Assets/script.js
+++ Assets/script.js
@@ -2,49 +2,83 @@ const rootURL = "https://api.openweathermap.org";
 const apiKey = "1dbbe04e7eaff33cabdcdc99e3464ff7&units=imperial";
 const search = document.getElementById("searchForWeather");
 const inputs = document.getElementsByClassName("inputs");
-let d = new Date();
-let date = d.toDateString();
 const currentOutputs = document.getElementsByClassName(
   "currentWeather-outputs"
 );
-let city = inputs.value;
 let temp = document.getElementById("temp");
 let humidity = document.getElementById("humidity");
 let windSpeed = document.getElementById("windSpeed");
 let weatherIcon = document.getElementById("weatherIcon");
 
+//Add event listener to search button
 search.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log("Button clicked!");
-  const currentData = `${rootURL}/data/2.5/weather?q=Durham&limit=5&appid=${apiKey}`; //!hardcoded city
-  console.log("button clicked", currentData);
+  let city = inputs[0].value;
+  const currentData = `${rootURL}/data/2.5/weather?q=${city}&limit=5&appid=${apiKey}`;
+  console.log("Button clicked!", currentData);
+  //Use fetch to get the current weather data from the openweathermap api
   fetch(currentData)
     .then((response) => response.json())
     .then((data) => {
-      if (data.cod == 200) {
+      if (data.cod === 200) {
+        displayCurrent(data);
+        saveSearchHistory(data);
         console.log("data", data);
       } else {
         console.log(data.message);
       }
-      displayCurrent(data);
-    }
-    );
-
-  function displayCurrent(data) {
-    if (data.message) {
-      console.log(data.message);
-    } else {
-        date;
-      temp.innerHTML = Math.round((parseFloat(data.main.temp))) + " °F";
-      humidity.innerHTML = data.main.humidity + " % humidity";
-      windSpeed.inerHTML = data.wind.speed + "mph";
-      weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
-    }
-
-    let searchResults = localStorage.getItem("searchResults") || "[]";
-    searchResults = JSON.parse(searchResults);
-    searchResults.push(data);
-    localStorage.setItem("searchResults", JSON.stringify(searchResults))
-    console.log("local storage", searchResults)
-    }
+    });
 });
+
+//Define the displayCurrent function to display the current weather data
+function displayCurrent(data) {
+  //Check if an error message was returned
+  if (data.message) {
+    //Log the error message to the console
+    console.log(data.message);
+  } else {
+    //Update the data
+    let d = new Date();
+    let date = d.toDateString();
+    temp.innerHTML = Math.round(parseFloat(data.main.temp)) + " °F";
+    humidity.innerHTML = data.main.humidity + " % humidity";
+    windSpeed.innerHTML = data.wind.speed + "mph";
+    weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+  }
+}
+
+//Define the saveSearchHistory function to save the search history
+function saveSearchHistory(data) {
+  //Get the search results from local storage, or an empty array if they don't exist
+  let searchResults = localStorage.getItem("searchResults") || "[]";
+  //parse search result data as a JavaScript Object using JSON.parse()
+  searchResults = JSON.parse(searchResults);
+  //Add new data to existing data by using push(data)
+  searchResults.push(data);
+  //store updated data back into local storage
+  localStorage.setItem("searchResults", JSON.stringify(searchResults));
+  console.log("local storage", searchResults);
+}
+
+//display previous search results
+function displayPrevious() {
+  const searchHistory = document.getElementsByClassName("searchHistory");
+  const searchHistoryBtn = document.getElementsByClassName("searchHistoryBtn");
+  const searchHistoryClearBtn =
+    document.getElementsByClassName("clearHistoryBtn");
+
+  for (let i = 0; i < searchResults.length; i++) {
+    searchHistoryBtn.addEventListener("click", function () {
+      const searchTerm = searchHistory[i].value;
+      searchHistoryClearBtn.innerHTML = "Clear Search History";
+      for (let j = 0; j < searchResults.length; j++) {
+        if (searchResults[j].includes(searchTerm)) {
+          const listItem = document.createElement("li");
+          listItem.textContent = searchResults[j];
+          searchHistory[i].appendChild(listItem);
+        }
+      }
+      displayPrevious();
+    });
+  }
+}
